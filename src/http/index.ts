63a82fd4dbe3d4717ff039ab540b2f2f6fccdc63
unknown <DoctorHays998@yandex.ru>
2023-12-28 01:12:22 +0300

    Fix api url

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NODE_ENV === 'production'?'https://ocsearch.ru:12345':'http://localhost:5000/api';
+const API_URL = process.env.NODE_ENV === 'production'?'https://ocsearch.ru:12345/api':'http://localhost:5000/api';
 
 export const api = axios.create({
     withCredentials: true,
