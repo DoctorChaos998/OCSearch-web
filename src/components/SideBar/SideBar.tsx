1cc44d3e2d1f78fe7ff37797a6f5f17d6cccda24
unknown <DoctorHays998@yandex.ru>
2024-01-10 18:18:51 +0300

    Started adapt for phone

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -9,23 +9,21 @@ import {logout} from "@/store/slices/userSlice/userActions";
 const SideBar: FC = () => {
     const dispatch = useAppDispatch();
     return (
-        <div className={classes.sideBarContainer}>
-            <aside className={classes.sideBarItems}>
-                <nav>
-                    <ul className={classes.sideBarItemsList}>
-                        <SideBarButton href={'/profile'} toolTipInscription='Profile' icon='account_circle'/>
-                        <SideBarButton href={'/search'} toolTipInscription='Search' icon='search'/>
-                        <SideBarButton href={'/databases'} toolTipInscription='Data bases' icon='storage'/>
-                        <SideBarButton href={'/folders'} toolTipInscription='File browser' icon='folder_open'/>
-                    </ul>
-                </nav>
-                <nav className={classes.sideBarBottom}>
-                    <ul className={classes.sideBarItemsList}>
-                        <SideBarButton href={'/login'} toolTipInscription='Logout' icon='logout' onClick={() => dispatch(logout())}/>
-                    </ul>
-                </nav>
-            </aside>
-        </div>
+        <aside className={classes.sideBarContainer}>
+            <nav>
+                <ul className={classes.sideBarItemsList}>
+                    <SideBarButton href={'/profile'} toolTipInscription='Profile' icon='account_circle'/>
+                    <SideBarButton href={'/search'} toolTipInscription='Search' icon='search'/>
+                    <SideBarButton href={'/databases'} toolTipInscription='Data bases' icon='storage'/>
+                    <SideBarButton href={'/folders'} toolTipInscription='File browser' icon='folder_open'/>
+                </ul>
+            </nav>
+            <nav className={classes.sideBarBottom}>
+                <ul className={classes.sideBarItemsList}>
+                    <SideBarButton href={'/login'} toolTipInscription='Logout' icon='logout' onClick={() => dispatch(logout())}/>
+                </ul>
+            </nav>
+        </aside>
     );
 };
 
--- src/ui/loaders/SessionsSkeletonLoading/SessionsSkeletonLoading.tsx
+++ src/ui/loaders/SessionsSkeletonLoading/SessionsSkeletonLoading.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Skeleton from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css'
+import classes from "./SessionsSkeletonLoading.module.scss";
 const SessionsSkeletonLoading = () => {
     return (
-        <Skeleton style={{marginTop:12}} baseColor="#202020" highlightColor="#833ab4" height={116} width={550} borderRadius={40}></Skeleton>
+        <div className={classes.loader}>
+            <Skeleton baseColor="#202020" highlightColor="#833ab4" width={'100%'} height={'100%'} borderRadius={40}></Skeleton>
+        </div>
     );
 };
 
